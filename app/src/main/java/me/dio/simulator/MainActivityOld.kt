8fbf2a22d4e4500e49c9db9d7296996fc4a9e3e2
gapigo <x.gabriel12@gmail.com>
2022-07-26 21:19:30 -0300

    Adding ActivityMainBinding and changing MainActivity class to java

---
+++ app/src/main/java/me/dio/simulator/MainActivityOld.kt
@@ -0,0 +1,21 @@
+package me.dio.simulator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator.databinding.ActivityMainBinding
+
+class MainActivityOld : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.activity_main)
+        //val tvhello= findViewById<TextView>(R.id.tvhello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.tvhello.text = "Teste"
+    }
+}

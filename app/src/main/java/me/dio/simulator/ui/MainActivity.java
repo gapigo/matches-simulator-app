97df6e3ee86b133d3859d48b1aa5c874e01dd7a4
gapigo <x.gabriel12@gmail.com>
2022-07-30 22:48:33 -0300

    Created package for data and initial api fetch

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -1,16 +1,31 @@
 package me.dio.simulator.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import me.dio.simulator.R;
+import me.dio.simulator.data.MatchesApi;
 import me.dio.simulator.databinding.ActivityMainBinding;
+import me.dio.simulator.domain.Match;
 import me.dio.simulator.domain.Team;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,9 +37,39 @@ public class MainActivity extends AppCompatActivity {
         //Team time = new Team("Teste", 5, "");
         //time.getImage();
 
+        setupHttpClient();
         setupMatchesList();
         setupMatchesRefresh();
         setupFloatingActionButton();
+    }
+
+    private void setupHttpClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://raw.githubusercontent.com/gapigo/matches-simulator-api/master/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Log.i("Log de debug", "Hello?");
+        matchesApi = retrofit.create(MatchesApi.class);
+    }
+
+    private void setupMatchesList() {
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if (response.isSuccessful()) {
+                    List<Match> matches = response.body();
+                    Log.i("MatchApiSuccess", "Deu tudo certo! Valtaram partidas: " + matches.size());
+                } else {
+                    Log.i("MatchApiError", response.raw().toString());
+                    showErrorMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+
+            }
+        });
     }
 
     private void setupFloatingActionButton() {
@@ -35,7 +80,8 @@ public class MainActivity extends AppCompatActivity {
         //TODO Atualizar as partidas na ação de swipe
     }
 
-    private void setupMatchesList() {
-        //TODO Listar as partidas, consumindo nossa API
+    private void showErrorMessage() {
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
+
 }
